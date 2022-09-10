be01d97ecef6d42c4d5f2118162db103fd34f4a5
MoinHasanfatta <112717999+MoinHasanfatta@users.noreply.github.com>
2022-09-10 15:52:36 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,7 @@
+
+let doc = document.getElementById("error")
+ 
+function purchase(){
+    console.log("The button clicks")
+    doc.textContent = "Sorry The Stocks are over!! Better luck next time"
+}
